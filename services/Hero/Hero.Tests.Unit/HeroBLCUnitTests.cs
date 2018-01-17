c9944f5a97cf44eb785cd002adbf111ea3c14cd8
mikejpoirier <mike.j.poirier@gmail.com>
2018-01-16 22:27:27 -0500

    updating unit tests

--- services/Hero/Hero.Tests.Unit/HeroBLCUnitTests.cs
+++ services/Hero/Hero.Tests.Unit/HeroBLCUnitTests.cs
@@ -8,97 +8,79 @@ namespace Hero
 {
     public class HeroBLCUnitTests
     {
-        private const string NEW = "new";
-        private const string EXISTING = "existing";
+        private Mock<IHeroDAO> _mockDAO;
         private HeroBLC _blc;
 
         public HeroBLCUnitTests()
         {
-            var daoMock = new Mock<IHeroDAO>();
-            daoMock.Setup(d => d.GetHeroes())
-                .Returns(new List<Hero>
-                {
-                    new Hero(),
-                    new Hero()
-                });
-
-            daoMock.Setup(d => d.GetHero(It.IsAny<string>()))
-                .Returns((string name) => 
-                {
-                    if(name == NEW)
-                        return null;
-                    else
-                        return new Hero 
-                            {  
-                                Name = name,
-                                Edition = "oldEdition"
-                            };
-                });
-
-            daoMock.Setup(d => d.InsertHero(It.IsAny<Hero>()))
-                .Returns((Hero hero) => hero);
-
-            daoMock.Setup(d => d.UpdateHero(It.IsAny<Hero>()))
-                .Returns((Hero hero) => hero);
-
-            _blc = new HeroBLC(daoMock.Object);
+            _mockDAO = new Mock<IHeroDAO>();
+            _blc = new HeroBLC(_mockDAO.Object);
         }
 
         [Fact]
         public void GetHeroes_ReturnsHeroes()
         {
+            _mockDAO.Setup(m => m.GetHeroes()).Returns(new List<Hero>());
+
             var result = _blc.GetHeroes();
 
-            Assert.True(result.Count > 1);
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void GetHero_ValidName_ReturnsHero()
         {
-            var result = _blc.GetHero(EXISTING);
+            _mockDAO.Setup(m => m.GetHero(It.IsAny<string>())).Returns(new Hero());
 
-            Assert.Equal(EXISTING, result.Name);
+            var result = _blc.GetHero("test");
+
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void PostHero_ExistingHero_UpdatesHero()
         {
-            var hero = new Hero
-            {
-                Name = EXISTING,
-                Edition = "newEdition"
-            };
+            var hero = new Hero();
+            _mockDAO.Setup(m => m.GetHero(It.IsAny<string>())).Returns(hero);
 
             var result = _blc.PostHero(hero);
 
-            Assert.Equal(hero.Edition, result.Edition);
+            _mockDAO.Verify(m => m.UpdateHero(hero));
         }
 
         [Fact]
         public void PostHero_NewHero_InsertsHero()
         {
-            var hero = new Hero
-            {
-                Name = NEW
-            };
+            var hero = new Hero();
+            _mockDAO.Setup(m => m.GetHero(It.IsAny<string>())).Returns((Hero)null);
 
             var result = _blc.PostHero(hero);
 
-            Assert.Equal(hero.Name, result.Name);
+            _mockDAO.Verify(m => m.InsertHero(hero));
         }
 
         [Fact]
-        public void PostHeroes_NewAndExistingHeroes_InsertsAndUpdatesHeroes()
+        public void PostHeroes_ExistingHero_UpdatesHero()
         {
-            var heroes = new List<Hero>
-            {
-                new Hero { Name = NEW, Edition = "Core" },
-                new Hero { Name = EXISTING, Edition = "Core" }
-            };
+            var hero = new Hero();
+            var heroes = new List<Hero> { hero };
+            _mockDAO.Setup(m => m.GetHero(It.IsAny<string>())).Returns(hero);
 
             var result = _blc.PostHeroes(heroes);
 
-            Assert.True(result.All(m => heroes.Any(mm => mm.Name == m.Name)));
+            _mockDAO.Verify(m => m.UpdateHero(hero));
+        }
+
+        [Fact]
+        public void PostHeroes_NewHero_InsertsHero()
+        {
+            var hero = new Hero();
+            var heroes = new List<Hero> { hero };
+            _mockDAO.Setup(m => m.GetHero(It.IsAny<string>())).Returns((Hero)null);
+
+            var result = _blc.PostHeroes(heroes);
+
+            _mockDAO.Verify(m => m.InsertHero(hero));
         }
     }
 }
