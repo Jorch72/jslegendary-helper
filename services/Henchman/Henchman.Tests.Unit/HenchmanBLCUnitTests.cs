c72a34848807c688fb7ead518c4442b7aa34d0e9
mikejpoirier <mike.j.poirier@gmail.com>
2018-01-16 22:17:27 -0500

    updating unit tests

--- services/Henchman/Henchman.Tests.Unit/HenchmanBLCUnitTests.cs
+++ services/Henchman/Henchman.Tests.Unit/HenchmanBLCUnitTests.cs
@@ -8,97 +8,79 @@ namespace Henchman
 {
     public class HenchmanBLCUnitTests
     {
-        private const string NEW = "new";
-        private const string EXISTING = "existing";
+        private Mock<IHenchmanDAO> _mockDAO;
         private HenchmanBLC _blc;
 
         public HenchmanBLCUnitTests()
         {
-            var daoMock = new Mock<IHenchmanDAO>();
-            daoMock.Setup(d => d.GetHenchmen())
-                .Returns(new List<Henchman>
-                {
-                    new Henchman(),
-                    new Henchman()
-                });
-
-            daoMock.Setup(d => d.GetHenchman(It.IsAny<string>()))
-                .Returns((string name) => 
-                {
-                    if(name == NEW)
-                        return null;
-                    else
-                        return new Henchman 
-                            {  
-                                Name = name,
-                                Edition = "oldEdition"
-                            };
-                });
-
-            daoMock.Setup(d => d.InsertHenchman(It.IsAny<Henchman>()))
-                .Returns((Henchman henchman) => henchman);
-
-            daoMock.Setup(d => d.UpdateHenchman(It.IsAny<Henchman>()))
-                .Returns((Henchman henchman) => henchman);
-
-            _blc = new HenchmanBLC(daoMock.Object);
+            _mockDAO = new Mock<IHenchmanDAO>();
+            _blc = new HenchmanBLC(_mockDAO.Object);
         }
 
         [Fact]
         public void GetHenchmen_ReturnsHenchmen()
         {
+            _mockDAO.Setup(m => m.GetHenchmen()).Returns(new List<Henchman>());
+
             var result = _blc.GetHenchmen();
 
-            Assert.True(result.Count > 1);
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void GetHenchman_ValidName_ReturnsHenchman()
         {
-            var result = _blc.GetHenchman(EXISTING);
+            _mockDAO.Setup(m => m.GetHenchman(It.IsAny<string>())).Returns(new Henchman());
 
-            Assert.Equal(EXISTING, result.Name);
+            var result = _blc.GetHenchman("test");
+
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void PostHenchman_ExistingHenchman_UpdatesHenchman()
         {
-            var henchman = new Henchman
-            {
-                Name = EXISTING,
-                Edition = "newEdition"
-            };
+            var henchman = new Henchman();
+            _mockDAO.Setup(m => m.GetHenchman(It.IsAny<string>())).Returns(henchman);
 
             var result = _blc.PostHenchman(henchman);
 
-            Assert.Equal(henchman.Edition, result.Edition);
+            _mockDAO.Verify(m => m.UpdateHenchman(henchman));
         }
 
         [Fact]
         public void PostHenchman_NewHenchman_InsertsHenchman()
         {
-            var henchman = new Henchman
-            {
-                Name = NEW
-            };
+            var henchman = new Henchman();
+            _mockDAO.Setup(m => m.GetHenchman(It.IsAny<string>())).Returns((Henchman)null);
 
             var result = _blc.PostHenchman(henchman);
 
-            Assert.Equal(henchman.Name, result.Name);
+            _mockDAO.Verify(m => m.InsertHenchman(henchman));
         }
 
         [Fact]
-        public void PostHenchmen_NewAndExistingHenchmen_InsertsAndUpdatesHenchmen()
+        public void PostHenchmen_ExistingHenchman_UpdatesHenchman()
         {
-            var henchmen = new List<Henchman>
-            {
-                new Henchman { Name = NEW, Edition = "Core" },
-                new Henchman { Name = EXISTING, Edition = "Core" }
-            };
+            var henchman = new Henchman();
+            var henchmen = new List<Henchman> { henchman };
+            _mockDAO.Setup(m => m.GetHenchman(It.IsAny<string>())).Returns(henchman);
 
             var result = _blc.PostHenchmen(henchmen);
 
-            Assert.True(result.All(m => henchmen.Any(mm => mm.Name == m.Name)));
+            _mockDAO.Verify(m => m.UpdateHenchman(henchman));
+        }
+
+        [Fact]
+        public void PostHenchmen_NewHenchman_InsertsHenchman()
+        {
+            var henchman = new Henchman();
+            var henchmen = new List<Henchman> { henchman };
+            _mockDAO.Setup(m => m.GetHenchman(It.IsAny<string>())).Returns((Henchman)null);
+
+            var result = _blc.PostHenchmen(henchmen);
+
+            _mockDAO.Verify(m => m.InsertHenchman(henchman));
         }
     }
 }
